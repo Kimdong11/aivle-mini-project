9be2da06ff6028e221f579876bf1b9f048a64aaf
김동현 <gimdonghyeon@gimdonghyeons-MacBook-Air.local>
2022-01-20 16:54:21 +0900

    prototype

--- app.js
+++ app.js
@@ -1,42 +1,11 @@
-const game1 = document.getElementById("game1");
-const game2 = document.getElementById("game2");
-const game3 = document.getElementById("game3");
-const game4 = document.getElementById("game4");
-const link = document.querySelector(".game-title")
-const image = document.getElementById("game-img")
-const a = document.createElement("a")
+const game1 = document.querySelector(".circle1")
+const gameText = document.querySelector(".circle-container h2")
 
-const chanegeContents = (event) => {
-    id = event.target.id;
-    if (id === "game1") {
-        a.text = "상식 퀴즈 입니다. 이동하시겠습니까?"
-        a.href = "./game1.html"
-        image.src = "./image/1.jpeg";
-        link.append(a)
-    }
-    else if (id === "game2") {
-        image.src = "./image/2.jpeg";
-        a.text = "속담 퀴즈 입니다. 이동하시겠습니까?"
-        a.href = "./game2.html"
-        link.appendChild(a)
-    }
-    else if (id === "game3") {
-        image.src = "./image/3.jpeg";
-        a.text = "영화 퀴즈 입니다. 이동하시겠습니까?"
-        a.href = "./game3.html"
-        link.appendChild(a)
-    }
-    else {
-        image.src = "./image/4.jpeg";
-        a.text = "랜덤 퀴즈 입니다. 이동하시겠습니까?"
-        a.href = "./game4.html"
-        link.appendChild(a)
-    }
+
+const handleMouseIn = (event) => {
+    console.log(event)
+    gameText.text = "Click Me"
 }
 
-
-game1.addEventListener('click', chanegeContents)
-game2.addEventListener("click", chanegeContents)
-game3.addEventListener("click", chanegeContents)
-game4.addEventListener("click", chanegeContents)
+game1.addEventListener("mouseenter", handleMouseIn)
 
