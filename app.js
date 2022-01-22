1303f8da8de8a9d696957ba5cd82b958777fbf50
김동현 <gimdonghyeon@gimdonghyeons-MacBook-Air.local>
2022-01-22 09:52:58 +0900

    Add github, eamil icon

--- app.js
+++ app.js
@@ -3,10 +3,10 @@ const earth = document.getElementById("earth");
 const moon = document.getElementById("moon");
 const saturn = document.getElementById("saturn");
 const jupyter = document.getElementById("jupyter");
-const earthText = document.querySelector(".image-container--earth");
-const moonText = document.querySelector(".image-container--moon");
-const saturnext = document.querySelector(".image-container--saturn");
-const jupyterText = document.querySelector(".image-container--jupyter");
+const earthText = document.querySelector(".earth-text");
+const moonText = document.querySelector(".moon-text");
+const saturnText = document.querySelector(".saturn-text");
+const jupyterText = document.querySelector(".jupyter-text");
 
 const class_name = ["earth", "moon", "saturn", "jupyter"];
 
@@ -22,6 +22,7 @@ const jupyter_css = document.styleSheets[1].rules[13].style;
 const handleMove = (e) => {
     const key = e.key;
     c_name = rocket.className;
+
     if (key == "ArrowRight" && left < 80.5) {
         rocket.style.left = `${left}%`;
         rocket.classList.replace(c_name, class_name[cnt]);
@@ -35,6 +36,9 @@ const handleMove = (e) => {
         cnt -= 1;
         rocket.style.left = `${left}%`;
     }
+    if (key === "Enter") {
+        console.log("다음 페이지로 이동!");
+    }
 };
 
 // Insert Animation Function
@@ -44,21 +48,37 @@ const imageAnimation = (name) => {
         moon_css.animation = "";
         saturn_css.animation = "";
         jupyter_css.animation = "";
+        earthText.innerText = "Press Enter";
+        moonText.innerText = "";
+        saturnText.innerText = "";
+        jupyterText.innerText = "";
     } else if (name == moon.className) {
         moon_css.animation = "1.5s ease-in-out 0s infinite normal none running imageMove";
         earth_css.animation = "";
         saturn_css.animation = "";
         jupyter_css.animation = "";
+        moonText.innerText = "Press Enter";
+        earthText.innerText = "";
+        saturnText.innerText = "";
+        jupyterText.innerText = "";
     } else if (name == saturn.className) {
         saturn_css.animation = "1.5s ease-in-out 0s infinite normal none running imageMove";
         earth_css.animation = "";
         moon_css.animation = "";
         jupyter_css.animation = "";
+        saturnText.innerText = "Press Enter";
+        earthText.innerText = "";
+        moonText.innerText = "";
+        jupyterText.innerText = "";
     } else {
         jupyter_css.animation = "1.5s ease-in-out 0s infinite normal none running imageMove";
         earth_css.animation = "";
         moon_css.animation = "";
         saturn_css.animation = "";
+        jupyterText.innerText = "Press Enter";
+        earthText.innerText = "";
+        moonText.innerText = "";
+        saturnText.innerText = "";
     }
 };
 
